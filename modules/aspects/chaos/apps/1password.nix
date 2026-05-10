f0a334c8d83fcf39e6d6a3b192668dbfb39a74e2
Fabian Sabau <git@fbsb.dev>
2026-05-10 21:15:00 +0200

    feat: add 1password

---
+++ modules/aspects/chaos/apps/1password.nix
@@ -0,0 +1,14 @@
+{
+  chaos,
+  ...
+}:
+{
+  chaos.all._.users.includes = [ chaos.apps._."1password" ];
+
+  chaos.apps._."1password" = {
+    nixos = {
+      programs._1password.enable = true;
+      programs._1password-gui.enable = true;
+    };
+  };
+}
