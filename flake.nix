4cf9db3c0172a3432e77acbf7d055622c7b637cc
Fabian Sabau <git@fbsb.dev>
2026-05-16 21:42:21 +0200

    chore: bump den to v0.16.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   inputs = {
-    den.url = "github:vic/den?ref=v0.15.0";
+    den.url = "github:vic/den?ref=v0.16.0";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
