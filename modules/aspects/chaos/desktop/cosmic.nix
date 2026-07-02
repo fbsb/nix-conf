0fb1380b5ddad0fb80099edb94278433ead06fe3
Fabian Sabau <git@fbsb.dev>
2026-07-02 23:08:29 +0200

    fix: workaround keyring unlock on cosmic
    When logging in with figerprint on cosmic
    the login keyring is notautomatically
    unlocked.
    
    Workaround this by forcing first login with
    a password instead of fingerprint.

--- modules/aspects/chaos/desktop/cosmic.nix
+++ modules/aspects/chaos/desktop/cosmic.nix
@@ -1,4 +1,11 @@
+{ ... }:
 {
+  chaos.desktop._.cosmic._.to-users = {
+    homeManager = {
+      services.gnome-keyring.enable = true;
+    };
+  };
+
   chaos.desktop._.cosmic = {
     nixos =
       {
@@ -10,10 +17,16 @@
         services.desktopManager.cosmic.enable = true;
         services.desktopManager.cosmic.xwayland.enable = true;
         services.system76-scheduler.enable = true;
+
+        services.gnome.gnome-keyring.enable = true;
         security.pam.services.login.enableGnomeKeyring = true;
+        security.pam.services.greetd.enableGnomeKeyring = true;
+        security.pam.services.greetd.fprintAuth = false;
 
         environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
         environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+        programs.seahorse.enable = true;
 
         environment.systemPackages = with pkgs; [
           cosmic-ext-applet-caffeine
