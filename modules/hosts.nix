492baa17808cffe2c995a088132e7c1e84072244
Fabian Sabau <git@fbsb.dev>
2026-08-08 23:45:52 +0200

    chore: switch case to kde

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -40,8 +40,7 @@ in
     chaos.all._.hosts
     chaos.hardware._.framework-13-amd-ai-300
     chaos.system._.keymap._.en-us-intl
-    chaos.desktop._.gnome
-    # chaos.desktop._.cosmic
+    chaos.desktop.kde
   ];
 
   den.aspects.fbsb.includes = [
